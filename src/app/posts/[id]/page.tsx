c7acd5ca6c30b7ed82b0227c2f8ef701387b6b90
Kens04 <kento090403@icloud.com>
2025-03-14 07:54:01 +0900

    単数系に変更

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 import { useCallback, useEffect, useState } from "react";
 
 const PostDetail = ({ params }: { params: { id: string } }) => {
-  const [posts, setPosts] = useState<Post>();
+  const [post, setPost] = useState<Post>();
   const [isLoading, setIsLoading] = useState<boolean>(true);
 
   const fetchData = useCallback(async () => {
@@ -14,7 +14,7 @@ const PostDetail = ({ params }: { params: { id: string } }) => {
         `https://1hmfpsvto6.execute-api.ap-northeast-1.amazonaws.com/dev/posts/${params.id}`
       );
       const data = (await res.json()) as PostResponse;
-      setPosts(data.post);
+      setPost(data.post);
     } catch (error) {
       console.log(error);
     } finally {
@@ -27,15 +27,15 @@ const PostDetail = ({ params }: { params: { id: string } }) => {
   }, [fetchData]);
 
   if (isLoading) return <div>読み込み中...</div>;
-  if (!posts) return <div>記事が見つかりません</div>;
+  if (!post) return <div>記事が見つかりません</div>;
 
   return (
     <div className="max-w-3xl mx-auto mt-10">
       <div>
         <div>
           <Image
-            src={posts.thumbnailUrl}
-            alt={posts.title}
+            src={post.thumbnailUrl}
+            alt={post.title}
             height={400}
             width={800}
           />
@@ -43,11 +43,11 @@ const PostDetail = ({ params }: { params: { id: string } }) => {
         <div className="p-4">
           <div className="flex justify-between">
             <div className="text-slate-400 text-sm">
-              {new Date(posts.createdAt).toLocaleDateString()}
+              {new Date(post.createdAt).toLocaleDateString()}
             </div>
             <div>
               <div className="flex gap-2">
-                {posts.categories.map((category, index) => (
+                {post.categories.map((category, index) => (
                   <div
                     key={index}
                     className="border border-blue-500 p-1 text-sm rounded text-blue-500"
@@ -59,10 +59,10 @@ const PostDetail = ({ params }: { params: { id: string } }) => {
             </div>
           </div>
           <p className="text-2xl text-left mt-4 text-slate-900">
-            {posts.title}
+            {post.title}
           </p>
           <div className="mt-4 text-left text-slate-700">
-            <div dangerouslySetInnerHTML={{ __html: posts.content }} />
+            <div dangerouslySetInnerHTML={{ __html: post.content }} />
           </div>
         </div>
       </div>
